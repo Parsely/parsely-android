ab587b6c955a323da4eebf1bf09206798684f050
Emmett Butler <emmett.butler321@gmail.com>
2013-04-09 13:03:20 -0400

    ported timer interaction to android project

--- HiParsely/src/com/example/hiparsely/MainActivity.java
+++ HiParsely/src/com/example/hiparsely/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import com.example.parselyandroid.*;
 
 public class MainActivity extends Activity {
 
@@ -12,11 +13,11 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		ParselyTracker.sharedInstance("arstechnica.com", 2);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
