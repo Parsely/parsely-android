90430dfeb6bb1a172d058cb57e7a562a61060b17
Emmett Butler <emmett.butler321@gmail.com>
2014-02-19 16:32:58 -0500

    update documentation

--- Documentation/html/search/classes_70.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['parselytracker',['ParselyTracker',['../classcom_1_1parsely_1_1parselyandroid_1_1_parsely_tracker.html',1,'com::parsely::parselyandroid']]]
-];
--- Documentation/html/search/functions_73.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['sharedinstance',['sharedInstance',['../classcom_1_1parsely_1_1parselyandroid_1_1_parsely_tracker.html#a7778f6160ee5e70fd5d61995fb5b90c8',1,'com::parsely::parselyandroid::ParselyTracker']]]
-];
