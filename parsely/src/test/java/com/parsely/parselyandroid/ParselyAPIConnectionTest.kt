de1eb9e20186bdb61fce16272608592024476173
Wojtek Zieba <wojtek.zieba@automattic.com>
2023-10-19 12:02:08 +0200

    tests: request without pixels case

--- parsely/src/test/java/com/parsely/parselyandroid/ParselyAPIConnectionTest.kt
+++ parsely/src/test/java/com/parsely/parselyandroid/ParselyAPIConnectionTest.kt
@@ -1,7 +1,12 @@
 package com.parsely.parselyandroid
 
+import android.content.Context
 import androidx.test.core.app.ApplicationProvider
+import okhttp3.mockwebserver.MockWebServer
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.After
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 
@@ -9,10 +14,25 @@ import org.robolectric.RobolectricTestRunner
 class ParselyAPIConnectionTest {
 
     private lateinit var sut: ParselyAPIConnection
+    private val mockServer = MockWebServer()
 
     @Before
     fun setUp() {
         sut = ParselyAPIConnection(FakeTracker)
+    }
+
+    @Test
+    fun `when making connection without any events, then make GET request`() {
+        // when
+        sut.execute(mockServer.url("/").toString())
+
+        // then
+        assertThat(mockServer.takeRequest().method).isEqualTo("GET")
+    }
+
+    @After
+    fun tearDown() {
+        mockServer.shutdown()
     }
 
     object FakeTracker : ParselyTracker(
