c997778ac5898245c920edd3709bd714bde3a7dd
Wojtek Zieba <wojtek.zieba@automattic.com>
2023-10-26 13:33:16 +0200

    tests: make interval calculator constants package-private
    So they can be used in unit tests

--- parsely/src/main/java/com/parsely/parselyandroid/UpdateEngagementIntervalCalculator.java
+++ parsely/src/main/java/com/parsely/parselyandroid/UpdateEngagementIntervalCalculator.java
@@ -6,9 +6,9 @@ import java.util.Calendar;
 
 class UpdateEngagementIntervalCalculator {
 
-    private static final long MAX_TIME_BETWEEN_HEARTBEATS = 60 * 60;
-    private static final long OFFSET_MATCHING_BASE_INTERVAL = 35;
-    private static final double BACKOFF_PROPORTION = 0.3;
+    static final long MAX_TIME_BETWEEN_HEARTBEATS = 60 * 60;
+    static final long OFFSET_MATCHING_BASE_INTERVAL = 35;
+    static final double BACKOFF_PROPORTION = 0.3;
 
     @NonNull private final Clock clock;
 
