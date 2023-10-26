f273ee142c647cfc6a6ee24b3d68bf98acc2734d
Wojtek Zieba <wojtek.zieba@automattic.com>
2023-10-26 14:58:32 +0200

    fix: incorrect interval calculation
    Refactor part: interval calculator now uses `kotlin.time.Duration`
    
    Thanks to this change we:
    - fix the bugs of unwanted rounding of `double` values to `long`
    - makes class easier to work, as we don't have to think about units with all the operation
    - makes implementation more similar to iOS, which uses `TimeInterval` (see: https://github.com/Parsely/AnalyticsSDK-iOS/blob/b1db4b05dcd5af6feb4710ec30d13fa704305692/Sources/Sampler.swift#L136)

--- parsely/src/main/java/com/parsely/parselyandroid/UpdateEngagementIntervalCalculator.kt
+++ parsely/src/main/java/com/parsely/parselyandroid/UpdateEngagementIntervalCalculator.kt
@@ -1,21 +1,29 @@
 package com.parsely.parselyandroid
 
 import java.util.Calendar
+import kotlin.time.Duration.Companion.hours
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.seconds
 
 internal open class UpdateEngagementIntervalCalculator(private val clock: Clock) {
 
     open fun updateLatestInterval(startTime: Calendar): Long {
-        val totalTrackedTime = (clock.now - startTime.time.time) / 1000
-        val totalWithOffset = (totalTrackedTime + OFFSET_MATCHING_BASE_INTERVAL).toDouble()
+        val startTimeDuration = startTime.time.time.milliseconds
+        val nowDuration = clock.now.milliseconds
+
+        val totalTrackedTime = nowDuration - startTimeDuration
+        val totalWithOffset = totalTrackedTime + offset
         val newInterval = totalWithOffset * BACKOFF_PROPORTION
-        val clampedNewInterval =
-            Math.min(MAX_TIME_BETWEEN_HEARTBEATS.toDouble(), newInterval).toLong()
-        return clampedNewInterval * 1000
+        val clampedNewInterval = minOf(maxTimeBetweenHeartbeats, newInterval)
+        return clampedNewInterval.inWholeMilliseconds
     }
 
     companion object {
         const val MAX_TIME_BETWEEN_HEARTBEATS = (60 * 60).toLong()
         const val OFFSET_MATCHING_BASE_INTERVAL: Long = 35
         const val BACKOFF_PROPORTION = 0.3
+
+        val offset = 35.seconds
+        val maxTimeBetweenHeartbeats = 1.hours
     }
 }
