729c40e3f35205d27cd086017bba8c9466d3ec91
Wojtek Zieba <wojtek.zieba@automattic.com>
2023-10-27 22:34:08 +0200

    tests: test case for "app tracks events above queue size limit"
    This commit adds test that checks if sending events over the limit of 50 events, works as intended. To not use flaky `Thread#sleep` approach, this test checks for:
    - local storage file changes via `java.nio.file.WatchService` API
    - finish of HTTP requests via `okhttp3.mockwebserver.takeRequest` API
    
    The `waitForFileEvents` method works in a way as `app.cash.turbine`, without a timeout though.

--- parsely/src/androidTest/java/com/parsely/parselyandroid/FunctionalTests.kt
+++ parsely/src/androidTest/java/com/parsely/parselyandroid/FunctionalTests.kt
@@ -4,12 +4,19 @@ import android.app.Activity
 import androidx.test.core.app.ActivityScenario
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import java.io.File
+import java.io.FileInputStream
+import java.io.ObjectInputStream
 import java.lang.reflect.Field
 import java.nio.file.Path
+import java.nio.file.StandardWatchEventKinds.ENTRY_CREATE
+import java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY
+import java.nio.file.WatchEvent
+import java.nio.file.WatchService
 import kotlin.io.path.Path
 import kotlin.time.Duration.Companion.seconds
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -30,6 +37,11 @@ class FunctionalTests {
         }
     }
 
+    /**
+     * In this scenario, the consumer application tracks more than 50 events-threshold during a flush interval.
+     * The SDK will save the events to disk and send them in the next flush interval.
+     * At the end, when all events are sent, the SDK will delete the content of local storage file.
+     */
     @Test
     fun appTracksEventsAboveQueueSizeLimit() {
         ActivityScenario.launch(SampleActivity::class.java).use { scenario ->
@@ -38,11 +50,66 @@ class FunctionalTests {
                 server.enqueue(MockResponse().setResponseCode(200))
                 parselyTracker = initializeTracker(activity)
 
-                parselyTracker.trackPageview("url", null, null, null)
+                repeat(51) {
+                    parselyTracker.trackPageview("url", null, null, null)
+                }
 
-                server.takeRequest()
+                // Waits for the SDK to save events to disk
+                val createLocalStorageEvents = appsFiles.waitForFileEvents(2)
+                assertThat(createLocalStorageEvents).satisfiesExactly(
+                    // Checks for local storage file creation
+                    { event -> assertThat(event.kind()).isEqualTo(ENTRY_CREATE) },
+                    // Checks if local storage file was modified
+                    { event -> assertThat(event.kind()).isEqualTo(ENTRY_MODIFY) },
+                )
+                assertThat(locallyStoredEvents).hasSize(51)
+            }
+
+            val dropLocalStorageEvent = appsFiles.waitForFileEvents(1)
+
+            // Waits for the SDK to send events (flush interval passes)
+            server.takeRequest()
+
+            assertThat(dropLocalStorageEvent).satisfiesExactly(
+                { event -> assertThat(event.kind()).isEqualTo(ENTRY_MODIFY) },
+            )
+            assertThat(locallyStoredEvents).hasSize(0)
+        }
+    }
+
+    private val locallyStoredEvents
+        get() = FileInputStream(File("$appsFiles/parsely-events.ser")).use {
+            ObjectInputStream(it).use { objectInputStream ->
+                @Suppress("UNCHECKED_CAST")
+                objectInputStream.readObject() as ArrayList<Map<String, Any>>
             }
         }
+
+
+    private fun Path.waitForFileEvents(numberOfEvents: Int): List<WatchEvent<*>> {
+        val service = watch()
+        val events = LinkedHashSet<WatchEvent<*>>()
+        while (true) {
+            val key = service.poll()
+            val polledEvents =
+                key?.pollEvents()?.filter { it.context().toString() == "parsely-events.ser" }
+                    .orEmpty()
+            events.addAll(polledEvents)
+            println("[Parsely] Caught ${events.size} file events")
+            if (events.size == numberOfEvents) {
+                key?.reset()
+                break
+            }
+            Thread.sleep(500)
+        }
+        return events.toList()
+    }
+
+    private fun Path.watch(): WatchService {
+        val watchService = this.fileSystem.newWatchService()
+        register(watchService, ENTRY_CREATE, ENTRY_MODIFY)
+
+        return watchService
     }
 
     private fun initializeTracker(activity: Activity): ParselyTracker {
