ed56a27b99ef0c45cdcd466516f089b780cf910a
Wojtek Zieba <wojtek.zieba@automattic.com>
2023-10-27 22:18:18 +0200

    tests: initialize ParselyTracker in functional test
    With address of mocked server as `ROOT_URL`, applied via reflection. `ROOT_URL` *must not* be a plain declaration, because otherwise the compiler will *inline* the value of the constant, and we won't be able to change it, even via reflection, for the functional test.

--- parsely/src/androidTest/java/com/parsely/parselyandroid/FunctionalTests.kt
+++ parsely/src/androidTest/java/com/parsely/parselyandroid/FunctionalTests.kt
@@ -3,6 +3,10 @@ package com.parsely.parselyandroid
 import android.app.Activity
 import androidx.test.core.app.ActivityScenario
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import java.lang.reflect.Field
+import kotlin.time.Duration.Companion.seconds
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -11,13 +15,36 @@ import org.junit.runner.RunWith
 class FunctionalTests {
 
     private lateinit var parselyTracker: ParselyTracker
+    private val server = MockWebServer()
+    private val url = server.url("/").toString()
 
     @Test
     fun appTracksEventsAboveQueueSizeLimit() {
         ActivityScenario.launch(SampleActivity::class.java).use { scenario ->
             scenario.onActivity { activity: Activity ->
+                server.enqueue(MockResponse().setResponseCode(200))
+                parselyTracker = initializeTracker(activity)
+
+                parselyTracker.trackPageview("url", null, null, null)
+
+                server.takeRequest()
             }
         }
+    }
+
+    private fun initializeTracker(activity: Activity): ParselyTracker {
+        return ParselyTracker.sharedInstance(
+            siteId, flushInterval.inWholeSeconds.toInt(), activity.application
+        ).apply {
+            val f: Field = this::class.java.getDeclaredField("ROOT_URL")
+            f.isAccessible = true
+            f.set(this, url)
+        }
+    }
+
+    private companion object {
+        const val siteId = "123"
+        val flushInterval = 10.seconds
     }
 
     class SampleActivity : Activity()
