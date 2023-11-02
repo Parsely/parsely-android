56a62b2b8f230261872299db0b0e7124e4d18376
Wojtek Zieba <wojtek.zieba@automattic.com>
2023-11-02 11:28:40 +0100

    tests: assert correct serialization of the Java file
    This test is added to assert correctness of serialization after migrating LocalStorageRepository from Java to Kotlin. Two languages might have some differences in how they serialize objects, so this test will assert that older storage files (created by Java) will have compatible content with new version of LocalStorageRepository, written in Kotlin.

--- parsely/src/test/java/com/parsely/parselyandroid/LocalStorageRepositoryTest.kt
+++ parsely/src/test/java/com/parsely/parselyandroid/LocalStorageRepositoryTest.kt
@@ -1,6 +1,8 @@
 package com.parsely.parselyandroid
 
+import android.content.Context
 import androidx.test.core.app.ApplicationProvider
+import java.io.File
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
 import org.junit.Test
@@ -11,10 +13,11 @@ import org.robolectric.RobolectricTestRunner
 class LocalStorageRepositoryTest {
 
     private lateinit var sut: LocalStorageRepository
+    private val context = ApplicationProvider.getApplicationContext<Context>()
 
     @Before
     fun setUp() {
-        sut = LocalStorageRepository(ApplicationProvider.getApplicationContext())
+        sut = LocalStorageRepository(context)
     }
 
     @Test
@@ -72,5 +75,49 @@ class LocalStorageRepositoryTest {
 
         // then
         assertThat(sut.storedQueue).isEmpty()
+    }
+
+    @Test
+    fun `given stored file with serialized events, when querying the queue, then list has expected events`() {
+        // given
+        val file = File(context.filesDir.path + "/parsely-events.ser")
+        File(ClassLoader.getSystemResource("valid-java-parsely-events.ser")?.path!!).copyTo(file)
+
+        // when
+        val queue = sut.storedQueue
+
+        // then
+        assertThat(queue).isEqualTo(
+            listOf(
+                mapOf(
+                    "idsite" to "example.com",
+                    "urlref" to "http://example.com/",
+                    "data" to mapOf<String, Any>(
+                        "manufacturer" to "Google",
+                        "os" to "android",
+                        "os_version" to "33",
+                        "parsely_site_uuid" to "b325e2c9-498c-4331-a967-2d6049317c77",
+                        "ts" to 1698918720863L
+                    ),
+                    "pvid" to "272cc2b8-5acc-4a70-80c7-20bb6eb843e4",
+                    "action" to "pageview",
+                    "url" to "http://example.com/article1.html"
+                ),
+                mapOf(
+                    "idsite" to "example.com",
+                    "urlref" to "http://example.com/",
+                    "data" to mapOf<String, Any>(
+                        "manufacturer" to "Google",
+                        "os" to "android",
+                        "os_version" to "33",
+                        "parsely_site_uuid" to "b325e2c9-498c-4331-a967-2d6049317c77",
+                        "ts" to 1698918742375L
+                    ),
+                    "pvid" to "e94567ec-3459-498c-bf2e-6a1b85ed5a82",
+                    "action" to "pageview",
+                    "url" to "http://example.com/article1.html"
+                )
+            )
+        )
     }
 }
