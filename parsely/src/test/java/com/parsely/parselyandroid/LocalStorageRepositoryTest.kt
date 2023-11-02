b137fbdfcda4ad6bed75df848014b1689542f8f9
Wojtek Zieba <wojtek.zieba@automattic.com>
2023-11-02 10:28:05 +0100

    tests: add unit tests coverage for LocalStorageRepository

--- parsely/src/test/java/com/parsely/parselyandroid/LocalStorageRepositoryTest.kt
+++ parsely/src/test/java/com/parsely/parselyandroid/LocalStorageRepositoryTest.kt
@@ -28,4 +28,49 @@ class LocalStorageRepositoryTest {
         // then
         assertThat(sut.storedQueue).hasSize(100)
     }
+
+    @Test
+    fun `given the list of events, when persisting the list, then querying the list returns the same result`() {
+        // given
+        val eventsList = (1..10).map { mapOf("index" to it) }
+
+        // when
+        sut.persistQueue(eventsList)
+
+        // then
+        assertThat(sut.storedQueue).hasSize(10).containsExactlyInAnyOrderElementsOf(eventsList)
+    }
+
+    @Test
+    fun `given no locally stored list, when requesting stored queue, then return an empty list`() {
+        assertThat(sut.storedQueue).isEmpty()
+    }
+
+    @Test
+    fun `given stored queue with some elements, when persisting in-memory queue, then assert there'll be no duplicates and queues will be combined`() {
+        // given
+        val storedQueue = (1..5).map { mapOf("index" to it) }
+        val inMemoryQueue = (3..10).map { mapOf("index" to it) }
+        sut.persistQueue(storedQueue)
+
+        // when
+        sut.persistQueue(inMemoryQueue)
+
+        // then
+        val expectedQueue = (1..10).map { mapOf("index" to it) }
+        assertThat(sut.storedQueue).hasSize(10).containsExactlyInAnyOrderElementsOf(expectedQueue)
+    }
+
+    @Test
+    fun `given stored queue, when purging stored queue, then assert queue is purged`() {
+        // given
+        val eventsList = (1..10).map { mapOf("index" to it) }
+        sut.persistQueue(eventsList)
+
+        // when
+        sut.purgeStoredQueue()
+
+        // then
+        assertThat(sut.storedQueue).isEmpty()
+    }
 }
