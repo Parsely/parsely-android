93657a77b3d33d55f4e50beba16dd7989b177746
Wojtek Zieba <wojtek.zieba@automattic.com>
2023-11-03 15:39:24 +0100

    tests: add unit test coverage for QueueManager

--- parsely/src/main/java/com/parsely/parselyandroid/QueueManager.java
+++ parsely/src/main/java/com/parsely/parselyandroid/QueueManager.java
@@ -5,8 +5,8 @@ import android.os.AsyncTask;
 import androidx.annotation.NonNull;
 
 class QueueManager extends AsyncTask<Void, Void, Void> {
-    private static final int QUEUE_SIZE_LIMIT = 50;
-    private static final int STORAGE_SIZE_LIMIT = 100;
+    static final int QUEUE_SIZE_LIMIT = 50;
+    static final int STORAGE_SIZE_LIMIT = 100;
 
     @NonNull
     private final ParselyTracker parselyTracker;
@@ -24,10 +24,10 @@ class QueueManager extends AsyncTask<Void, Void, Void> {
     @Override
     protected Void doInBackground(Void... params) {
         // if event queue is too big, push to persisted storage
-        if (parselyTracker.eventQueue.size() > QUEUE_SIZE_LIMIT) {
+        if (parselyTracker.getInMemoryQueue().size() > QUEUE_SIZE_LIMIT) {
             ParselyTracker.PLog("Queue size exceeded, expelling oldest event to persistent memory");
-            localStorageRepository.persistQueue(parselyTracker.eventQueue);
-            parselyTracker.eventQueue.remove(0);
+            localStorageRepository.persistQueue(parselyTracker.getInMemoryQueue());
+            parselyTracker.getInMemoryQueue().remove(0);
             // if persisted storage is too big, expel one
             if (parselyTracker.storedEventsCount() > STORAGE_SIZE_LIMIT) {
                 localStorageRepository.expelStoredEvent();
