8496ab7f94bea658f7dd33970a968f06a9bb37a2
Wojtek Zieba <wojtek.zieba@automattic.com>
2023-11-07 19:22:44 +0100

    feat: check the buffer every second
    To reduce overhead of constant loop, the `InMemoryBuffer` will check `buffer` list every second.

--- parsely/src/main/java/com/parsely/parselyandroid/InMemoryBuffer.kt
+++ parsely/src/main/java/com/parsely/parselyandroid/InMemoryBuffer.kt
@@ -1,6 +1,9 @@
 package com.parsely.parselyandroid
 
+import kotlin.time.Duration.Companion.seconds
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
@@ -15,13 +18,14 @@ internal class InMemoryBuffer(
 
     init {
         coroutineScope.launch {
-            while (true) {
+            while (isActive) {
                 mutex.withLock {
                     if (buffer.isNotEmpty()) {
                         localStorageRepository.insertEvents(buffer)
                         buffer.clear()
                     }
                 }
+                delay(1.seconds)
             }
         }
     }
