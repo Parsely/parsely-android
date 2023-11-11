4237fcdfa59c2b8ed9139f5c170be7eb9b491fe4
Wojtek Zieba <wojtek.zieba@automattic.com>
2023-11-11 19:55:18 +0100

    fix: add mutual execution for `SendEvents#invoke`
    Make the `SendEvents` run once at a time, to prevent a scenario, when we send multiple requests with the same events. In current state of SDK, it could happen when `FlushManager` counts to next flush interval **and** user moves app to the background at the same time.

--- parsely/src/main/java/com/parsely/parselyandroid/SendEvents.kt
+++ parsely/src/main/java/com/parsely/parselyandroid/SendEvents.kt
@@ -3,6 +3,8 @@ package com.parsely.parselyandroid
 import com.parsely.parselyandroid.JsonSerializer.toParselyEventsPayload
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 
 internal class SendEvents(
     private val flushManager: FlushManager,
@@ -11,39 +13,43 @@ internal class SendEvents(
     private val scope: CoroutineScope
 ) {
 
+    private val mutex = Mutex()
+
     operator fun invoke(isDebug: Boolean) {
         scope.launch {
-            val eventsToSend = localStorageRepository.getStoredQueue()
+            mutex.withLock {
+                val eventsToSend = localStorageRepository.getStoredQueue()
 
-            if (eventsToSend.isEmpty()) {
-                return@launch
-            }
-            ParselyTracker.PLog("Sending request with %d events", eventsToSend.size)
+                if (eventsToSend.isEmpty()) {
+                    return@launch
+                }
+                ParselyTracker.PLog("Sending request with %d events", eventsToSend.size)
 
-            val jsonPayload = toParselyEventsPayload(eventsToSend)
+                val jsonPayload = toParselyEventsPayload(eventsToSend)
 
-            ParselyTracker.PLog("POST Data %s", jsonPayload)
+                ParselyTracker.PLog("POST Data %s", jsonPayload)
 
-            if (isDebug) {
-                ParselyTracker.PLog("Debug mode on. Not sending to Parse.ly")
-                localStorageRepository.remove(eventsToSend)
-            } else {
-                ParselyTracker.PLog("Requested %s", ParselyTracker.ROOT_URL)
-                parselyAPIConnection.send(jsonPayload)
-                    .fold(
-                        onSuccess = {
-                            ParselyTracker.PLog("Pixel request success")
-                            localStorageRepository.remove(eventsToSend)
-                            ParselyTracker.PLog("Event queue empty, flush timer cleared.")
-                            if (localStorageRepository.getStoredQueue().isEmpty()) {
-                                flushManager.stop()
+                if (isDebug) {
+                    ParselyTracker.PLog("Debug mode on. Not sending to Parse.ly")
+                    localStorageRepository.remove(eventsToSend)
+                } else {
+                    ParselyTracker.PLog("Requested %s", ParselyTracker.ROOT_URL)
+                    parselyAPIConnection.send(jsonPayload)
+                        .fold(
+                            onSuccess = {
+                                ParselyTracker.PLog("Pixel request success")
+                                localStorageRepository.remove(eventsToSend)
+                                ParselyTracker.PLog("Event queue empty, flush timer cleared.")
+                                if (localStorageRepository.getStoredQueue().isEmpty()) {
+                                    flushManager.stop()
+                                }
+                            },
+                            onFailure = {
+                                ParselyTracker.PLog("Pixel request exception")
+                                ParselyTracker.PLog(it.toString())
                             }
-                        },
-                        onFailure = {
-                            ParselyTracker.PLog("Pixel request exception")
-                            ParselyTracker.PLog(it.toString())
-                        }
-                    )
+                        )
+                }
             }
         }
     }
