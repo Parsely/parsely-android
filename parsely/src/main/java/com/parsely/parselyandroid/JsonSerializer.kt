268b0374b7c3a712464e21e78437f51e29f49985
Wojtek Zieba <wojtek.zieba@automattic.com>
2023-11-11 19:15:34 +0100

    refactor: move serialization details to `JsonSerializer`
    `SendEvents` shouldn't know details about structure of JSON payload.

--- parsely/src/main/java/com/parsely/parselyandroid/JsonSerializer.kt
+++ parsely/src/main/java/com/parsely/parselyandroid/JsonSerializer.kt
@@ -5,13 +5,19 @@ import java.io.IOException
 import java.io.StringWriter
 
 internal object JsonSerializer {
+
+    fun toParselyEventsPayload(eventsToSend: List<Map<String, Any?>?>): String {
+        val batchMap: MutableMap<String, Any> = HashMap()
+        batchMap["events"] = eventsToSend
+        return toJson(batchMap).orEmpty()
+    }
     /**
      * Encode an event Map as JSON.
      *
      * @param map The Map object to encode as JSON.
      * @return The JSON-encoded value of `map`.
      */
-    fun toJson(map: Map<String, Any>): String? {
+    private fun toJson(map: Map<String, Any>): String? {
         val mapper = ObjectMapper()
         var ret: String? = null
         try {
