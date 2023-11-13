153e160fd13a99ccc9f62c46fca2076a32764749
Wojtek Zieba <wojtek.zieba@automattic.com>
2023-11-13 15:09:30 +0100

    feat: use Coroutines to enqueue events in `EngagementManager`
    This commits has minimal set of changes that moves implementation towards usage of Kotlin Coroutines with passing unit test. It also makes tests not time-sensitive.

--- parsely/src/main/java/com/parsely/parselyandroid/Clock.kt
+++ parsely/src/main/java/com/parsely/parselyandroid/Clock.kt
@@ -2,9 +2,10 @@ package com.parsely.parselyandroid
 
 import java.util.Calendar
 import java.util.TimeZone
+import kotlin.time.Duration
 import kotlin.time.Duration.Companion.milliseconds
 
 open class Clock {
-    open val now
+    open val now: Duration
         get() = Calendar.getInstance(TimeZone.getTimeZone("UTC")).timeInMillis.milliseconds
 }
