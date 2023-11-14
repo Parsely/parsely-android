59a83d8c5b216095ea2450a3a4c3a996bddb36a2
Wojtek Zieba <wojtek.zieba@automattic.com>
2023-11-14 16:21:38 +0100

    tests: add unit tests for `UuidProvider`

---
+++ parsely/src/test/java/com/parsely/parselyandroid/UuidProviderTest.kt
@@ -0,0 +1,56 @@
+package com.parsely.parselyandroid
+
+import android.app.Application
+import android.provider.Settings
+import androidx.test.core.app.ApplicationProvider
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+internal class UuidProviderTest {
+
+    lateinit var sut: UuidProvider
+
+    @Before
+    fun setUp() {
+        sut = UuidProvider(ApplicationProvider.getApplicationContext())
+    }
+
+    @Test
+    fun `given no site uuid is stored, when requesting uuid, then return ANDROID_ID value`() {
+        // given
+        val fakeAndroidId = "test id"
+        Settings.Secure.putString(
+            ApplicationProvider.getApplicationContext<Application>().contentResolver,
+            Settings.Secure.ANDROID_ID,
+            fakeAndroidId
+        )
+
+        // when
+        val result=  sut.provide()
+
+        // then
+        assertThat(result).isEqualTo(fakeAndroidId)
+    }
+
+    @Test
+    fun `given site uuid already requested, when requesting uuid, then return same uuid`() {
+        // given
+        val fakeAndroidId = "test id"
+        Settings.Secure.putString(
+            ApplicationProvider.getApplicationContext<Application>().contentResolver,
+            Settings.Secure.ANDROID_ID,
+            fakeAndroidId
+        )
+        val storedValue = sut.provide()
+
+        // when
+        val result = sut.provide()
+
+        // then
+        assertThat(result).isEqualTo(storedValue)
+    }
+}
