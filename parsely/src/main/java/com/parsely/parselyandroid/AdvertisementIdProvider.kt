5c037592f61641bfa23ec6dabb17ca44177cbb07
Wojtek Zieba <wojtek.zieba@automattic.com>
2023-11-14 16:09:39 +0100

    refactor: extract getting uuid to `UuidProvider`
    It encapsulates all logic related to retrieving uuid

--- parsely/src/main/java/com/parsely/parselyandroid/AdvertisementIdProvider.kt
+++ parsely/src/main/java/com/parsely/parselyandroid/AdvertisementIdProvider.kt
@@ -1,6 +1,8 @@
 package com.parsely.parselyandroid
 
 import android.content.Context
+import android.content.SharedPreferences
+import android.provider.Settings
 import com.google.android.gms.ads.identifier.AdvertisingIdClient
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
@@ -24,6 +26,53 @@ internal class AdvertisementIdProvider(
     }
 
     override fun provide(): String? = adKey
+}
+
+internal class UuidProvider(private val context: Context) : IdProvider {
+    private val settings: SharedPreferences = context.getSharedPreferences("parsely-prefs", 0)
+
+    private val siteUuid: String?
+        /**
+         * Get the UUID for this user.
+         */
+        get() {
+            var uuid: String? = ""
+            try {
+                uuid = settings.getString(UUID_KEY, "")
+                if (uuid == "") {
+                    uuid = generateSiteUuid()
+                }
+            } catch (ex: Exception) {
+                ParselyTracker.PLog(
+                    "Exception caught during site uuid generation: %s",
+                    ex.toString()
+                )
+            }
+            return uuid
+        }
+
+    /**
+     * Read the Parsely UUID from application context or make a new one.
+     *
+     * @return The UUID to use for this user.
+     */
+    private fun generateSiteUuid(): String {
+        val uuid = Settings.Secure.getString(
+            context.applicationContext.contentResolver,
+            Settings.Secure.ANDROID_ID
+        )
+        ParselyTracker.PLog(String.format("Generated UUID: %s", uuid))
+        return uuid
+    }
+
+    override fun provide(): String? {
+        return siteUuid
+    }
+
+    companion object {
+        private const val UUID_KEY = "parsely-uuid"
+    }
+
 }
 
 internal fun interface IdProvider {
--- parsely/src/main/java/com/parsely/parselyandroid/DeviceInfoRepository.kt
+++ parsely/src/main/java/com/parsely/parselyandroid/DeviceInfoRepository.kt
@@ -1,19 +1,15 @@
 package com.parsely.parselyandroid
 
-import android.content.Context
-import android.content.SharedPreferences
 import android.os.Build
-import android.provider.Settings
 
 internal interface DeviceInfoRepository{
     fun collectDeviceInfo(): Map<String, String>
 }
 
 internal open class AndroidDeviceInfoRepository(
-    private val context: Context,
-    private val advertisementIdProvider: IdProvider
+    private val advertisementIdProvider: IdProvider,
+    private val uuidProvider: IdProvider,
 ): DeviceInfoRepository {
-    private val settings: SharedPreferences = context.getSharedPreferences("parsely-prefs", 0)
 
     /**
      * Collect device-specific info.
@@ -36,6 +32,7 @@ internal open class AndroidDeviceInfoRepository(
     private val parselySiteUuid: String
         get() {
             val adKey = advertisementIdProvider.provide()
+            val siteUuid = uuidProvider.provide()
 
             ParselyTracker.PLog("adkey is: %s, uuid is %s", adKey, siteUuid)
 
@@ -46,42 +43,4 @@ internal open class AndroidDeviceInfoRepository(
                 siteUuid .orEmpty()
             }
         }
-
-    private val siteUuid: String?
-        /**
-         * Get the UUID for this user.
-         */
-        get() {
-            var uuid: String? = ""
-            try {
-                uuid = settings.getString(UUID_KEY, "")
-                if (uuid == "") {
-                    uuid = generateSiteUuid()
-                }
-            } catch (ex: Exception) {
-                ParselyTracker.PLog(
-                    "Exception caught during site uuid generation: %s",
-                    ex.toString()
-                )
-            }
-            return uuid
-        }
-
-    /**
-     * Read the Parsely UUID from application context or make a new one.
-     *
-     * @return The UUID to use for this user.
-     */
-    private fun generateSiteUuid(): String {
-        val uuid = Settings.Secure.getString(
-            context.applicationContext.contentResolver,
-            Settings.Secure.ANDROID_ID
-        )
-        ParselyTracker.PLog(String.format("Generated UUID: %s", uuid))
-        return uuid
-    }
-
-    companion object {
-        private const val UUID_KEY = "parsely-uuid"
-    }
 }
