19cd5c8738826c2c4121354a99d15519a06ece76
Wojtek Zieba <wojtek.zieba@automattic.com>
2023-11-14 11:42:39 +0100

    refactor: move `DeviceInfoRepository` to Kotlin

--- parsely/src/main/java/com/parsely/parselyandroid/DeviceInfoRepository.kt
+++ parsely/src/main/java/com/parsely/parselyandroid/DeviceInfoRepository.kt
@@ -1,124 +1,122 @@
-package com.parsely.parselyandroid;
+package com.parsely.parselyandroid
 
-import static com.parsely.parselyandroid.ParselyTracker.PLog;
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.AsyncTask
+import android.os.Build
+import android.provider.Settings
+import com.google.android.gms.ads.identifier.AdvertisingIdClient
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException
+import com.google.android.gms.common.GooglePlayServicesRepairableException
+import java.io.IOException
 
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.os.AsyncTask;
-import android.provider.Settings;
+internal class DeviceInfoRepository(private val context: Context) {
+    private var adKey: String? = null
+    private val settings: SharedPreferences = context.getSharedPreferences("parsely-prefs", 0)
 
-import androidx.annotation.NonNull;
-
-import com.google.android.gms.ads.identifier.AdvertisingIdClient;
-import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
-import com.google.android.gms.common.GooglePlayServicesRepairableException;
-
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
-class DeviceInfoRepository {
-
-    private static final String UUID_KEY = "parsely-uuid";
-    private String adKey = null;
-    @NonNull
-    private final Context context;
-    private final SharedPreferences settings;
-
-    DeviceInfoRepository(@NonNull Context context) {
-        this.context = context;
-        settings = context.getSharedPreferences("parsely-prefs", 0);
-        new GetAdKey(context).execute();
+    init {
+        GetAdKey(context).execute()
     }
 
     /**
      * Collect device-specific info.
-     * <p>
+     *
+     *
      * Collects info about the device and user to use in Parsely events.
      */
-    Map<String, String> collectDeviceInfo() {
-        Map<String, String> dInfo = new HashMap<>();
+    fun collectDeviceInfo(): Map<String, String> {
+        val dInfo: MutableMap<String, String> = HashMap()
 
         // TODO: screen dimensions (maybe?)
-        dInfo.put("parsely_site_uuid", getParselySiteUuid());
-        dInfo.put("manufacturer", android.os.Build.MANUFACTURER);
-        dInfo.put("os", "android");
-        dInfo.put("os_version", String.format("%d", android.os.Build.VERSION.SDK_INT));
+        dInfo["parsely_site_uuid"] = parselySiteUuid
+        dInfo["manufacturer"] = Build.MANUFACTURER
+        dInfo["os"] = "android"
+        dInfo["os_version"] = String.format("%d", Build.VERSION.SDK_INT)
 
         // FIXME: Not passed in event or used anywhere else.
-        CharSequence txt = context.getPackageManager().getApplicationLabel(context.getApplicationInfo());
-        dInfo.put("appname", txt.toString());
-
-        return dInfo;
+        val txt = context.packageManager.getApplicationLabel(context.applicationInfo)
+        dInfo["appname"] = txt.toString()
+        return dInfo
     }
 
-    private String getParselySiteUuid() {
-        PLog("adkey is: %s, uuid is %s", adKey, getSiteUuid());
-        final String uuid = (adKey != null) ? adKey : getSiteUuid();
-        return uuid;
-    }
-
-    /**
-     * Get the UUID for this user.
-     */
-    //TODO: docs about where we get this UUID from and how.
-    private String getSiteUuid() {
-        String uuid = "";
-        try {
-            uuid = settings.getString(UUID_KEY, "");
-            if (uuid.equals("")) {
-                uuid = generateSiteUuid();
-            }
-        } catch (Exception ex) {
-            PLog("Exception caught during site uuid generation: %s", ex.toString());
+    private val parselySiteUuid: String
+        get() {
+            ParselyTracker.PLog("adkey is: %s, uuid is %s", adKey, siteUuid)
+            return if (adKey != null) adKey!! else siteUuid!!
         }
-        return uuid;
-    }
+
+    private val siteUuid: String?
+        /**
+         * Get the UUID for this user.
+         */
+        get() {
+            var uuid: String? = ""
+            try {
+                uuid = settings.getString(UUID_KEY, "")
+                if (uuid == "") {
+                    uuid = generateSiteUuid()
+                }
+            } catch (ex: Exception) {
+                ParselyTracker.PLog(
+                    "Exception caught during site uuid generation: %s",
+                    ex.toString()
+                )
+            }
+            return uuid
+        }
 
     /**
      * Read the Parsely UUID from application context or make a new one.
      *
      * @return The UUID to use for this user.
      */
-    private String generateSiteUuid() {
-        String uuid = Settings.Secure.getString(context.getApplicationContext().getContentResolver(),
-                Settings.Secure.ANDROID_ID);
-        PLog(String.format("Generated UUID: %s", uuid));
-        return uuid;
+    private fun generateSiteUuid(): String {
+        val uuid = Settings.Secure.getString(
+            context.applicationContext.contentResolver,
+            Settings.Secure.ANDROID_ID
+        )
+        ParselyTracker.PLog(String.format("Generated UUID: %s", uuid))
+        return uuid
     }
+
     /**
      * Async task to get adKey for this device.
      */
-    private class GetAdKey extends AsyncTask<Void, Void, String> {
-        private final Context mContext;
-
-        public GetAdKey(Context context) {
-            mContext = context;
-        }
-
-        @Override
-        protected String doInBackground(Void... params) {
-            AdvertisingIdClient.Info idInfo = null;
-            String advertId = null;
+    private inner class GetAdKey(private val mContext: Context) :
+        AsyncTask<Void?, Void?, String?>() {
+        protected override fun doInBackground(vararg params: Void?): String? {
+            var idInfo: AdvertisingIdClient.Info? = null
+            var advertId: String? = null
             try {
-                idInfo = AdvertisingIdClient.getAdvertisingIdInfo(mContext);
-            } catch (GooglePlayServicesRepairableException | IOException |
-                     GooglePlayServicesNotAvailableException | IllegalArgumentException e) {
-                PLog("No Google play services or error! falling back to device uuid");
+                idInfo = AdvertisingIdClient.getAdvertisingIdInfo(mContext)
+            } catch (e: GooglePlayServicesRepairableException) {
+                ParselyTracker.PLog("No Google play services or error! falling back to device uuid")
                 // fall back to device uuid on google play errors
-                advertId = getSiteUuid();
+                advertId = siteUuid
+            } catch (e: IOException) {
+                ParselyTracker.PLog("No Google play services or error! falling back to device uuid")
+                advertId = siteUuid
+            } catch (e: GooglePlayServicesNotAvailableException) {
+                ParselyTracker.PLog("No Google play services or error! falling back to device uuid")
+                advertId = siteUuid
+            } catch (e: IllegalArgumentException) {
+                ParselyTracker.PLog("No Google play services or error! falling back to device uuid")
+                advertId = siteUuid
             }
             try {
-                advertId = idInfo.getId();
-            } catch (NullPointerException e) {
-                advertId = getSiteUuid();
+                advertId = idInfo!!.id
+            } catch (e: NullPointerException) {
+                advertId = siteUuid
             }
-            return advertId;
+            return advertId
         }
 
-        @Override
-        protected void onPostExecute(String advertId) {
-            adKey = advertId;
+        override fun onPostExecute(advertId: String?) {
+            adKey = advertId
         }
+    }
+
+    companion object {
+        private const val UUID_KEY = "parsely-uuid"
     }
 }
