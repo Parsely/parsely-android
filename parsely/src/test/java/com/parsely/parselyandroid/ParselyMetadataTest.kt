597ff4ce9c77f04a0a6d32dc08c0985225cde17f
Wojtek Zieba <wojtek.zieba@automattic.com>
2023-11-29 18:46:12 +0100

    tests: add unit test for `ParselyVideoMetaData#toMap` method

--- parsely/src/test/java/com/parsely/parselyandroid/ParselyMetadataTest.kt
+++ parsely/src/test/java/com/parsely/parselyandroid/ParselyMetadataTest.kt
@@ -9,13 +9,6 @@ class ParselyMetadataTest {
     @Test
     fun `given metadata with complete set of data, when converting to map, then the map is as expected`() {
         // given
-        val authors = arrayListOf("first author", "second author")
-        val link = "sample link"
-        val section = "sample section"
-        val tags = arrayListOf("first tag", "second tag")
-        val thumbUrl = "sample thumb url"
-        val title = "sample title"
-        val pubDate = Calendar.getInstance().apply { set(2023, 0, 1) }
         val sut = ParselyMetadata(
             authors,
             link,
@@ -30,16 +23,48 @@ class ParselyMetadataTest {
         val map = sut.toMap()
 
         // then
-        assertThat(map).isEqualTo(
-            mapOf(
-                "authors" to authors,
-                "link" to link,
-                "section" to section,
-                "tags" to tags,
-                "thumb_url" to thumbUrl,
-                "title" to title,
-                "pub_date_tmsp" to pubDate.timeInMillis / 1000
-            )
+        assertThat(map).isEqualTo(expectedParselyMetadataMap)
+    }
+
+    @Test
+    fun `given video metadata with complete set of data, when converting to map, then the map is as expected`() {
+        // given
+        val duration = 12
+        val sut = ParselyVideoMetadata(
+            authors,
+            link,
+            section,
+            tags,
+            thumbUrl,
+            title,
+            pubDate,
+            duration
+        )
+
+        // when
+        val map = sut.toMap()
+
+        // then
+        assertThat(map).isEqualTo(expectedParselyMetadataMap + ("duration" to duration))
+    }
+
+    companion object {
+        val authors = arrayListOf("first author", "second author")
+        val link = "sample link"
+        val section = "sample section"
+        val tags = arrayListOf("first tag", "second tag")
+        val thumbUrl = "sample thumb url"
+        val title = "sample title"
+        val pubDate = Calendar.getInstance().apply { set(2023, 0, 1) }
+
+        val expectedParselyMetadataMap = mapOf(
+            "authors" to authors,
+            "link" to link,
+            "section" to section,
+            "tags" to tags,
+            "thumb_url" to thumbUrl,
+            "title" to title,
+            "pub_date_tmsp" to pubDate.timeInMillis / 1000
         )
     }
 }
