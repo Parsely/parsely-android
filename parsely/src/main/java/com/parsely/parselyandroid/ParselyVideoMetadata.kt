8ae5bdb57eb949a3295a7704712bf5e1d7332624
Wojtek Zieba <wojtek.zieba@automattic.com>
2023-11-29 19:11:14 +0100

    refactor: migrate `ParselyMetadata` and `ParselyVideoMetadata` to kotlin

--- parsely/src/main/java/com/parsely/parselyandroid/ParselyVideoMetadata.kt
+++ parsely/src/main/java/com/parsely/parselyandroid/ParselyVideoMetadata.kt
@@ -1,53 +1,38 @@
-package com.parsely.parselyandroid;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Map;
+package com.parsely.parselyandroid
 
 /**
  * ParselyMetadata for video content.
  */
-public class ParselyVideoMetadata extends ParselyMetadata {
-
-    int durationSeconds;
-
-    /**
-     * Create a new ParselyVideoMetadata object.
-     *
-     * @param authors         The names of the authors of the video. Up to 10 authors are accepted.
-     * @param videoId         Unique identifier for the video. Required.
-     * @param section         The category or vertical to which this video belongs.
-     * @param tags            User-defined tags for the video. Up to 20 are allowed.
-     * @param thumbUrl        URL at which the main image for this video is located.
-     * @param title           The title of the video.
-     * @param publicationDateMilliseconds         The timestamp in milliseconds this video was published.
-     * @param durationSeconds Duration of the video in seconds. Required.
-     */
-    public ParselyVideoMetadata(
-            @Nullable ArrayList<String> authors,
-            @NonNull String videoId,
-            @Nullable String section,
-            @Nullable ArrayList<String> tags,
-            @Nullable String thumbUrl,
-            @Nullable String title,
-            long publicationDateMilliseconds,
-            int durationSeconds
-    ) {
-        super(authors, videoId, section, tags, thumbUrl, title, publicationDateMilliseconds);
-        this.durationSeconds = durationSeconds;
-    }
-
+class ParselyVideoMetadata
+/**
+ * Create a new ParselyVideoMetadata object.
+ *
+ * @param authors         The names of the authors of the video. Up to 10 authors are accepted.
+ * @param videoId         Unique identifier for the video. Required.
+ * @param section         The category or vertical to which this video belongs.
+ * @param tags            User-defined tags for the video. Up to 20 are allowed.
+ * @param thumbUrl        URL at which the main image for this video is located.
+ * @param title           The title of the video.
+ * @param publicationDateMilliseconds         The timestamp in milliseconds this video was published.
+ * @param durationSeconds Duration of the video in seconds. Required.
+ */(
+    authors: ArrayList<String>?,
+    videoId: String,
+    section: String?,
+    tags: ArrayList<String>?,
+    thumbUrl: String?,
+    title: String?,
+    publicationDateMilliseconds: Long,
+    @JvmField internal val durationSeconds: Int
+) : ParselyMetadata(authors, videoId, section, tags, thumbUrl, title, publicationDateMilliseconds) {
     /**
      * Turn this object into a Map
      *
      * @return a Map object representing the metadata.
      */
-    Map<String, Object> toMap() {
-        Map<String, Object> output = super.toMap();
-        output.put("duration", this.durationSeconds);
-        return output;
+    override fun toMap(): Map<String, Any?>? {
+        val output = super.toMap()?.toMutableMap()
+        output?.put("duration", durationSeconds)
+        return output
     }
 }
